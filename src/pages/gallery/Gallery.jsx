68c62051c26629047f65d2f9c58d68bf6427abed
Sandile Ndimande <105316897+Sandile-Dev01@users.noreply.github.com>
2023-11-01 12:07:53 +0200

    Update Gallery.jsx

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./gallery.css";
 import Image_1 from "./assets/Img_1.jpg";
+import Image_2 from "./assets/Img_2.jpg";
+import Image_3 from "./assets/Img_3.jpg";
+import Image_4 from "./assets/Img_4.jpg";
+import Image_5 from "./assets/Img_5.jpg";
 
 
 const Gallery = () => {
@@ -17,16 +21,16 @@ const Gallery = () => {
         <img src={Image_1} alt="Image 1" />
     </div>
     <div className="grid-item">
-        <img src={Image_1} alt="Image 2" />
+        <img src={Image_2} alt="Image 2" />
     </div>
    <div className="grid-item">
-        <img src={Image_1} alt="Image 1" />
+        <img src={Image_3} alt="Image 1" />
     </div>
     <div className="grid-item">
-        <img src={Image_1} alt="Image 2" />
+        <img src={Image_4} alt="Image 2" />
     </div>
    <div className="grid-item">
-        <img src={Image_1} alt="Image 1" />
+        <img src={Image_5} alt="Image 1" />
     </div>
     <div className="grid-item">
         <img src={Image_1} alt="Image 2" />
