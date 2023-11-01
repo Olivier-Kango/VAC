0b6a72597f5b89589dadf7eafd33ecbb954a43a5
Sandile Ndimande <105316897+Sandile-Dev01@users.noreply.github.com>
2023-11-01 14:30:33 +0200

    Update Gallery.jsx

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -5,16 +5,16 @@ import Image_2 from "./assets/Img_2.jpg";
 import Image_3 from "./assets/Img_3.jpg";
 import Image_4 from "./assets/Img_4.jpg";
 import Image_5 from "./assets/Img_5.jpg";
-import Image_6 from "./assets/Img_1.jpg";
-import Image_7 from "./assets/Img_2.jpg";
-import Image_8 from "./assets/Img_3.jpg";
-import Image_9 from "./assets/Img_4.jpg";
-import Image_10 from "./assets/Img_5.jpg";
-import Image_11 from "./assets/Img_1.jpg";
-import Image_12 from "./assets/Img_2.jpg";
-import Image_13 from "./assets/Img_3.jpg";
-import Image_14 from "./assets/Img_4.jpg";
-import Image_15 from "./assets/Img_5.jpg";
+import Image_6 from "./assets/Img_6.jpg";
+import Image_7 from "./assets/Img_7.jpg";
+import Image_8 from "./assets/Img_8.jpg";
+import Image_9 from "./assets/Img_9.jpg";
+import Image_10 from "./assets/Img_10.jpg";
+import Image_11 from "./assets/Img_11.jpg";
+import Image_12 from "./assets/Img_12.jpg";
+import Image_13 from "./assets/Img_13.jpg";
+import Image_14 from "./assets/Img_14.jpg";
+import Image_15 from "./assets/Img_15.jpg";
 
 
 const Gallery = () => {
@@ -43,26 +43,36 @@ const Gallery = () => {
         <img src={Image_5} alt="Image 1" />
     </div>
     <div className="grid-item">
-        <img src={Image_1} alt="Image 2" />
+        <img src={Image_6} alt="Image 2" />
     </div>
    <div className="grid-item">
-        <img src={Image_1} alt="Image 1" />
+        <img src={Image_7} alt="Image 1" />
     </div>
     <div className="grid-item">
-        <img src={Image_1} alt="Image 2" />
+        <img src={Image_8} alt="Image 2" />
     </div>
    <div className="grid-item">
-        <img src={Image_1} alt="Image 1" />
+        <img src={Image_9} alt="Image 1" />
     </div>
     <div className="grid-item">
-        <img src={Image_1} alt="Image 2" />
+        <img src={Image_10} alt="Image 2" />
     </div>
    <div className="grid-item">
-        <img src={Image_1} alt="Image 1" />
+        <img src={Image_11} alt="Image 1" />
     </div>
     <div className="grid-item">
-        <img src={Image_1} alt="Image 2" />
+        <img src={Image_12} alt="Image 2" />
     </div>
+
+     <div className="grid-item">
+        <img src={Image_13} alt="Image 1" />
+    </div>
+    <div className="grid-item">
+        <img src={Image_14} alt="Image 2" />
+    </div>
+     <div className="grid-item">
+        <img src={Image_15} alt="Image 1" />
+    </div
 </div>
 
     </>
