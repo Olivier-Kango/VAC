c601bd058c138067d7cf08e829ede3446e09948a
Sandile Ndimande <105316897+Sandile-Dev01@users.noreply.github.com>
2023-11-01 08:05:25 +0200

    Update Gallery.jsx

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -6,20 +6,21 @@ import Image_1 from "./assets/Img_1.jpg";
 const Gallery = () => {
   return (
     <>
-      <div className="gallery_header">
-        <h1>Gallery</h1>
-        <p>See how we have impacted Africa</p>
-      </div>
-      <div className="gallery_content">
+      
      
-           <div className="grid-item">
-            <img src={Image_1} alt="Image 1">
-        </div>
-        <div className="grid-item">
-            <img src={Image_1} alt="Image 2">
-        </div>
-     
-      </div>
+    <div className="gallery_header">
+    <h1>Gallery</h1>
+    <p>See how we have impacted Africa</p>
+</div>
+<div className="gallery_content">
+    <div className="grid-item">
+        <img src={Image_1} alt="Image 1" />
+    </div>
+    <div className="grid-item">
+        <img src={Image_2} alt="Image 2" />
+    </div>
+</div>
+
     </>
   );
 };
